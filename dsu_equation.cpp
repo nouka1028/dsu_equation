a7e06ea2d7961375642883d8485561e0127df69d
nouka1729 <116523837+nouka1729@users.noreply.github.com>
2023-12-08 22:04:07 +0900

    Update dsu_equation.cpp

--- dsu_equation.cpp
+++ dsu_equation.cpp
@@ -1,197 +1,170 @@
 template<class T>
-class frac{
-    T bunsi,bunbo;
-    constexpr void reduct()noexcept{
-        T g=gcd(bunsi,bunbo);
-        bunsi/=g;bunbo/=g;
-        if(bunbo<0){
-            bunsi=-bunsi;bunbo=-bunbo;
-        }
+struct Fraction{
+    T Bunsi,Bunbo;
+    Fraction(T Bunsi=0,T Bunbo=1)noexcept:Bunsi(Bunsi),Bunbo(Bunbo){reduce();}
+    void reduce(){
+        if(Bunbo<0){Bunbo=-Bunbo;Bunsi=-Bunsi;}
+        if(Bunsi==0){Bunbo=1;return;}
+        T GCD=gcd(abs(Bunsi),abs(Bunbo));
+        Bunsi/=GCD;Bunbo/=GCD;
     }
-public:
-    constexpr frac(T Bunsi=0,T Bunbo=1)noexcept{
-        bunsi=Bunsi;bunbo=Bunbo;
-        reduct();
+    const Fraction<T>& operator++(){
+        Bunsi+=Bunbo;reduce();return *this;
     }
-    constexpr T &Bunsi(){return bunsi;}
-    constexpr T &Bunbo(){return bunbo;}
-    constexpr frac<T>&operator+=(const frac<T>&rhs)noexcept{
-        bunsi=bunsi*rhs.bunbo+bunbo*rhs.bunsi;
-        bunbo*=rhs.bunbo;
-        reduct();
+    const Fraction<T>& operator--(){
+        Bunsi-=Bunbo;reduce();return *this;
+    }
+    const Fraction<T>& operator+=(const Fraction<T>& rhs)noexcept{
+        Bunsi=Bunsi*rhs.Bunbo+Bunbo*rhs.Bunsi;
+        Bunbo=Bunbo*rhs.Bunbo;reduce();
         return *this;
     }
-    constexpr frac<T>&operator-=(const frac<T>&rhs)noexcept{
-        bunsi=bunsi*rhs.bunbo-bunbo*rhs.bunsi;
-        bunbo*=rhs.bunbo;
-        reduct();
+    const Fraction<T>& operator-=(const Fraction<T>& rhs)noexcept{
+        Bunsi=Bunsi*rhs.Bunbo-Bunbo*rhs.Bunsi;
+        Bunbo=Bunbo*rhs.Bunbo;reduce();
         return *this;
     }
-    constexpr frac<T>&operator*=(const frac<T>&rhs)noexcept{
-        bunbo*=rhs.bunbo;
-        bunsi*=rhs.bunsi;
-        reduct();
+    const Fraction<T>& operator*=(const Fraction<T>& rhs)noexcept{
+        Bunsi=Bunsi*rhs.Bunsi;
+        Bunbo=Bunbo*rhs.Bunbo;reduce();
         return *this;
     }
-    constexpr frac<T>&operator/=(const frac<T>&rhs)noexcept{
-        bunbo*=rhs.bunsi;
-        bunsi*=rhs.bunbo;
-        reduct();
+    const Fraction<T>& operator/=(const Fraction<T>& rhs)noexcept{
+        Bunsi*=rhs.Bunbo;
+        Bunbo*=rhs.Bunsi;reduce();
         return *this;
     }
-    constexpr frac<T>operator+(const frac<T>&rhs)const noexcept{return frac(*this)+=rhs;}
-    constexpr frac<T>operator-(const frac<T>&rhs)const noexcept{return frac(*this)-=rhs;}
-    constexpr frac<T>operator*(const frac<T>&rhs)const noexcept{return frac(*this)*=rhs;}
-    constexpr frac<T>operator/(const frac<T>&rhs)const noexcept{return frac(*this)/=rhs;}
-    constexpr bool operator<(const frac<T>&rhs)const noexcept{return bunsi*rhs.bunbo<bunbo*rhs.bunsi;}
-    constexpr bool operator>(const frac<T>&rhs)const noexcept{return bunsi*rhs.bunbo>bunbo*rhs.bunsi;}
-    constexpr bool operator>=(const frac<T>&rhs)const noexcept{return bunsi*rhs.bunbo>=bunbo*rhs.bunsi;}
-    constexpr bool operator<=(const frac<T>&rhs)const noexcept{return bunsi*rhs.bunbo<=bunbo*rhs.bunsi;}
-    constexpr bool operator==(const frac<T>&rhs)const noexcept{return bunsi*rhs.bunbo==bunbo*rhs.bunsi;}
-    constexpr bool operator!=(const frac<T>&rhs)const noexcept{return bunsi*rhs.bunbo!=bunbo*rhs.bunsi;}
+
+    friend Fraction<T> operator+(const Fraction<T> &lhs,const Fraction<T> &rhs)noexcept{
+        return Fraction(lhs)+=rhs;
+    }
+    friend Fraction<T> operator-(const Fraction<T> &lhs,const Fraction<T> &rhs)noexcept{
+        return Fraction(lhs)-=rhs;
+    }
+    friend Fraction<T> operator*(const Fraction<T> &lhs,const Fraction<T> &rhs)noexcept{
+        return Fraction(lhs)*=rhs;
+    }
+    friend Fraction<T> operator/(const Fraction<T> &lhs,const Fraction<T> &rhs)noexcept{
+        return Fraction(lhs)/=rhs;
+    }
+    constexpr bool operator<(const Fraction<T> &rhs)const noexcept{
+        return Bunsi*rhs.Bunbo<Bunbo*rhs.Bunsi;
+    }
+    constexpr bool operator<=(const Fraction<T> &rhs)const noexcept{
+        return Bunsi*rhs.Bunbo<=Bunbo*rhs.Bunsi;
+    }
+    constexpr bool operator>(const Fraction<T> &rhs)const noexcept{
+        return Bunsi*rhs.Bunbo>Bunbo*rhs.Bunsi;
+    }
+    constexpr bool operator>=(const Fraction<T> &rhs)const noexcept{
+        return Bunsi*rhs.Bunbo>=Bunbo*rhs.Bunsi;
+    }
+    constexpr bool operator==(const Fraction<T> &rhs)const noexcept{
+        return Bunsi*rhs.Bunbo==Bunbo*rhs.Bunsi;
+    }
+    constexpr bool operator!=(const Fraction<T> &rhs)const noexcept{
+        return Bunsi*rhs.Bunbo!=Bunbo*rhs.Bunsi;
+    }
+    
 };
 
-#define frac frac<int>
+
 
 struct dsu_equation{
-    private:
-    vector<int> par;
-    vector<int> siz;
-    vector<frac> coef_x,coef_c;
-    vector<bool> isvaild;
-    vector<frac> val;
-    public:
-    dsu_equation(int n=1){
+private:
+    using frac=Fraction<long long>;
+    vector<int> par; //parent
+    vector<int> siz; //size
+    vector<frac> coef_p,coef_q; //coefficient
+    vector<bool> specify_; //is value uniquely determined
+    vector<frac> val; //value
+public:
+    dsu_equation(int n=0){
         par.resize(n);
         siz.resize(n);
-        coef_x.resize(n);
-        coef_c.resize(n);
-        isvaild.resize(n);
+        coef_p.resize(n);
+        coef_q.resize(n);
+        specify_.resize(n);
         val.resize(n);
-        for(int i=0;i<n;i++)par[i]=i;
         for(int i=0;i<n;i++){
+            par[i]=i;
             siz[i]=1;
-            coef_x[i]=1;
-            coef_c[i]=0;
         }
     }
-    int root(int x){
+
+    //Find a leader of the group contains x
+    int leader(int x){
         if(par[x]==x)return x;
-        return root(par[x]);
+        return leader(par[x]);
     }
-    
+
+    //Find the size of the group contains x
     int size(int x){
-        return siz[root(x)];
+        return siz[leader(x)];
     }
 
-    // A_rx=pA_x+q となるp,qを返す
-    pair<frac,frac> relationship(int x,frac cx=1,frac cc=0){
-        if(par[x]==x)return make_pair(cx,cc);
-        return relationship(par[x],cx*coef_x[x],cc*coef_x[x]+coef_c[x]);
-    }
-
+    //Check if x y are in the same group
     bool same(int x,int y){
-        return root(x)==root(y);
-    }
-
-    //ax+by=cとして結合
-    int merge(int x,int y,int a,int b,int c){
-        if(a==0||b==0){
-            if(a==0&&b==0){
-                if(c==0)return 1;
-                else return 0;
-            }else if(a!=0){
-                frac res=c/a;
-                int rx=root(x);
-                pair<frac,frac> lx=relationship(x);
-                if(isvaild[rx]&&lx.first*res+lx.second!=val[rx])return 0;
-                else{
-                    isvaild[rx]=true;
-                    val[rx]=lx.first*res+lx.second;
-                    return 1;
-                }
-            }else if(b!=0){
-                frac res=c/b;
-                int ry=root(y);
-                pair<frac,frac> ly=relationship(y);
-                if(isvaild[ry]&&ly.first*res+ly.second!=val[ry])return 0;
-                else{
-                    isvaild[ry]=true;
-                    val[ry]=ly.first*res+ly.second;
-                    return 1;
-                }
-            }
-        }
-        //xを根に持ってくる
-        if(same(x,y)){
-            pair<frac,frac> lx=relationship(x);
-            pair<frac,frac> ly=relationship(y);
-            int rx=root(x);
-            if(isvaild[rx]){
-                frac cx=frac(a)/lx.first+frac(b)/ly.first;
-                frac cc=frac(c)+frac(a)*lx.second/lx.first+frac(b)*ly.second/ly.first;
-                if(cx==0){
-                    if(cc==0)return 1;
-                    else return 0;
-                }
-                else if(val[rx]==cc/cx)return 1;
-                else return 0;
-            }else{
-                frac cx=frac(a)/lx.first+frac(b)/ly.first;
-                frac cc=frac(c)+frac(a)*lx.second/lx.first+frac(b)*ly.second/ly.first;
-                if(cx==0){
-                    if(cc==0)return 1;
-                    else return 0;
-                }
-                else{
-                    isvaild[rx]=true;
-                    val[rx]=cc/cx;
-                    return 1;
-                }
-            }
-        }
-        if(size(x)<size(y)){
-            swap(x,y);swap(a,b);
-        }
-        pair<frac,frac> lx=relationship(x);
-        pair<frac,frac> ly=relationship(y);
-        int rx=root(x);
-        int ry=root(y);
-        if(isvaild[ry]){
-            frac res=(frac(c)-frac(b)*val[ry]/ly.first+frac(b)*ly.second/ly.first+frac(a)*lx.second/lx.first)*lx.first/a;
-            if(isvaild[rx]&&res!=val[rx])return 0;
-            isvaild[rx]=true;
-            val[rx]=res;
-        }
-        par[ry]=rx;
-        siz[rx]+=siz[ry];
-        coef_x[ry]=(frac(-b)*lx.first)/(frac(a)*ly.first);
-        coef_c[ry]=(frac(c)+frac(b)*ly.second/ly.first+frac(a)*lx.second/lx.first)*lx.first/a;
-        return 1;
-    }
-
-    pair<bool,int> getval(int x){
-        pair<frac,frac> lx=relationship(x);
-        int rx=root(x);
-        if(!isvaild[rx])return make_pair(false,0);
-        else{
-            frac res=(val[rx]-lx.second)/lx.first;
-            return make_pair(true,res.Bunsi());
-        }
+        return leader(x)==leader(y);
     }
     
-    pair<int,int> calc(int x,int y,int xval){
-        auto xv=getval(x);
-        auto yv=getval(y);
-        if(xv.first&&yv.first)return make_pair(1,yv.second);
-        if(yv.first)return make_pair(2,yv.second);
-        if(same(x,y)){
-            pair<frac,frac> lx=relationship(x),ly=relationship(y);
-            frac res=(lx.first*xval+lx.second-ly.second)/ly.first;
-            return make_pair(3,res.Bunsi());
+    //return rootx(leader of x) , p , q satisfying A[rootx] = p * A[x] + q 
+    tuple<int,frac,frac> relationship(int x,frac p=1,frac q=0){
+        if(par[x]==x)return {x,p,q};
+        return relationship(par[x],p*coef_p[x],q*coef_p[x]+coef_q[x]);
+    }
+
+    //unite as a * A[x] + b * A[y] = c
+    //return false if contradiction else true
+    //must satisfy a != 0 and b != 0
+    bool merge(int x,int y,int a,int b,int c){
+        assert(a!=0&&b!=0);
+        auto [rx,px,qx]=relationship(x);
+        auto [ry,py,qy]=relationship(y);
+        if(specify_[rx]&&specify_[ry]){
+            return (a*(val[rx]/px-qx/px)+b*(val[ry]/py-qy/py)==c);
+        }
+        if(rx==ry){
+            //value is determined uniquely or other
+            frac ca=a/px+b/py,cb=c+a*qx/px+b*qy/py;
+            if(ca==0)return cb==0;
+            specify_[rx]=true;
+            val[rx]=cb/ca;
+            return true;
         }else{
-            return make_pair(4,0);
+            if(siz[rx]<siz[ry]){
+                swap(rx,ry),swap(a,b),swap(px,py),swap(qx,qy);
+            }
+            par[ry]=rx;
+            siz[rx]+=siz[ry];
+            coef_p[ry]=(-b*px)/(a*py);
+            coef_q[ry]=(qx+b*px*qy/(a*py)+(c*px)/a);
+            if(specify_[ry]){
+                val[rx]=coef_p[ry]*val[ry]+coef_q[ry];
+            }
+            return true;
         }
     }
 
+    //set as A[x]=f , return false if contradiction else true
+    bool set_val(int x,frac f){
+        auto [rx,px,qx]=relationship(x);
+        if(specify_[rx])return px*f+qx==val[rx];
+        else {val[rx]=px*f+qx;return specify_[rx]=true;}
+    }
+    
+    //return {is A[x] uniquely determined ?, its value}
+    pair<bool,frac> specify(int x){
+        auto [rx,px,qx]=relationship(x);
+        if(!specify_[rx])return {false,0};
+        return {true,val[rx]/px-qx/px};
+    }
+
+    //return {p,q} satisfying A[y] = p * A[x] + q 
+    tuple<bool,frac,frac> solve_coef(int x,int y){
+        auto [rx,px,qx]=relationship(x);
+        auto [ry,py,qy]=relationship(y);
+        if(rx!=ry)return {false,0,0};
+        return {true,px/py,(qx-qy)/py};
+    }
 };
